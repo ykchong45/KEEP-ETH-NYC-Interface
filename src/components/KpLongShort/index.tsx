f023ef856b24778347024410220e9d32656f3a43
ykchong45 <ykchong45@gmail.com>
2022-06-26 17:20:30 +0800

    call margin write function

--- src/components/KpLongShort/index.tsx
+++ src/components/KpLongShort/index.tsx
@@ -6,8 +6,15 @@ import KpInfoList from '@/components/KpInfoList';
 import { useWeb3React } from '@web3-react/core';
 import KpTokenInput from '@/components/KpTokenInput';
 import KpTokenSelect from '@/components/KpTokenSelect';
+import useSWR from 'swr';
+import { checkAllowance, approve, performTx, withConfirmation } from '@/apis';
+import { ethers } from 'ethers';
+import { parseUnits } from '@ethersproject/units';
+import LendingPool from '@/abis/LendingPool.json';
+import { toBN } from '@/utils';
 
 import styles from './index.less';
+import { getPoolAddr, getToken } from '@/constants';
 
 const KpLongShort = (props: any) => {
   const {
@@ -25,9 +32,12 @@ const KpLongShort = (props: any) => {
   } = props;
   const [more, setMore] = useState(false);
   const [lm, setLm] = useState(true);
-  const { chainId, active, account, activate, deactivate } = useWeb3React();
+  const { library, chainId, active, account, activate, deactivate } =
+    useWeb3React();
   const [slider, setSlider] = useState(1);
   const [inputVal, setInputVal] = useState();
+  const [step, setStep] = useState('approve');
+  const [buttonText, setButtonText] = useState('Approvee');
 
   // TODO: calculate info
   const [infolistMain, setInfolistMain] = useState([]);
@@ -66,6 +76,99 @@ const KpLongShort = (props: any) => {
       inputVal,
       slider,
     );
+  };
+
+  // const token = collateralToken ? getToken(chainId, collateralToken) || 'ETH';
+  const poolAddr = getPoolAddr('Main Pool');
+
+  // allowance check
+  const { data: allowance, mutate: updateAllowance } = useSWR(
+    active &&
+      collateralToken && [
+        chainId,
+        library,
+        account,
+        collateralToken?.name || 'ETH',
+        poolAddr,
+      ],
+    checkAllowance,
+  );
+  console.log('kplongshort, allowance: ', allowance);
+
+  useEffect(() => {
+    console.log('kpbuy, update step', inputVal, allowance);
+    if (!inputVal || !allowance) return;
+    const inputBN = ethers.BigNumber.from(`${inputVal}`);
+    if (inputBN.gt(allowance)) {
+      setStep('approve');
+    } else {
+      setStep('tx');
+    }
+  }, [allowance, inputVal]);
+
+  useEffect(() => {
+    if (!library) return;
+    library.on('block', () => {
+      console.log('kpbuy, updated block');
+      updateAllowance();
+    });
+    return () => {
+      library.removeAllListeners('block');
+    };
+  }, [library]);
+
+  // button click action
+  const onButtonClicked = () => {
+    console.log('executingTX');
+    const poolAddr = getPoolAddr('Main Pool');
+    if (step == 'approve') {
+      approve(chainId, library, account, collateralToken.name, poolAddr);
+    } else {
+      const parsedAmount = parseUnits(
+        inputVal || `0`,
+        collateralToken.decimals,
+      ).toString();
+      const parsedLeverage = parseUnits(`${slider}` || `0`, 27).toString();
+      if (selectedTab == 'Long') {
+        withConfirmation(
+          performTx(
+            library,
+            LendingPool.abi,
+            account,
+            poolAddr,
+            'openPosition',
+            [
+              collateralToken.address,
+              futureToken.address,
+              collateralToken.address,
+              parsedAmount,
+              parsedLeverage,
+            ],
+          ),
+        ).then(() => {
+          console.log('Transaction done');
+        });
+      } else if (selectedTab == 'Short') {
+        withConfirmation(
+          performTx(
+            library,
+            LendingPool.abi,
+            account,
+            poolAddr,
+            'openPosition',
+            [
+              futureToken.address,
+              collateralToken.address,
+              futureToken.address,
+              parsedAmount,
+              parsedLeverage,
+            ],
+          ),
+        ).then(() => {
+          console.log('Transaction done');
+        });
+      }
+    }
   };
 
   return (
@@ -116,11 +219,11 @@ const KpLongShort = (props: any) => {
           {lm && (
             <Slider
               onChange={(e) => setSlider(e)}
-              marks={{ 1: '1x', 2: '2x', 3: '3x', 4: '4x', 5: '5x' }}
+              marks={{ 1: '1x', 5: '5x', 10: '10x', 15: '15x', 20: '20x' }}
               value={slider}
               step={0.1}
               min={1}
-              max={5}
+              max={20}
             />
           )}
         </>
@@ -128,8 +231,8 @@ const KpLongShort = (props: any) => {
         <KpInfoList dataSource={infolistMain} />
 
         <div style={{ textAlign: 'center', marginTop: '20px' }}>
-          <Button disabled={!active} onClick={printArgs}>
-            {selectedTab}
+          <Button disabled={!active} onClick={onButtonClicked}>
+            {step == 'approve' ? 'Approve' : selectedTab}
           </Button>
         </div>
       </div>
